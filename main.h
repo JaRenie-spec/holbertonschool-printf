f47775909a0dd8ff429732986ba9aaf672ef6001
JaRenie-spec <renie.jeanalain@gmail.com>
2024-11-25 15:11:12 +0100

    adding struct and prototype for char,string and modulo

--- main.h
+++ main.h
@@ -1,2 +1,18 @@
+#ifndef _GUARD_H
+#define _GUARD_H
+#include <stdarg.h>
+#include <stddef.h>
+
+
+typedef struct type
+{
+	char *spec;
+	void (*f)(va_list ap);
+}print_type;
+
 int _putchar(char c);
 int _printf(const char *format, ...);
+void pchar(va_list ap);
+void pstring(va_list ap);
+void pmodulo(va_list ap);
+#endif
